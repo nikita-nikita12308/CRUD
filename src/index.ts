044942d773227168f5944157f156bcf7ea344bc1
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-06-25 20:49:58 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express, {Application, Request, Response} from "express";
+
+class App {
+    public app: Application;
+
+    constructor(){
+        this.app = express();
+        this.routes()
+    }
+
+    protected routes():void{
+        this.app.route("/").get((req:Request, res:Response) => {
+            res.send("Welcome Home")
+        });
+    }
+}
+
+const port:number = 8000
+
+const app = new App().app
+
+app.listen(port, () => {
+    console.log("Server listening on port")
+})
