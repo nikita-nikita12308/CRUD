61762ed31020f66275234101d2f49a9c93526d2c
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-06-26 00:52:26 +0300

    db connect

--- build/index.js
+++ build/index.js
@@ -4,10 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const database_1 = __importDefault(require("./config/database"));
 class App {
     constructor() {
         this.app = (0, express_1.default)();
         this.routes();
+    }
+    databaseSync() {
+        var _a;
+        const db = new database_1.default();
+        (_a = db.sequelize) === null || _a === void 0 ? void 0 : _a.sync();
     }
     routes() {
         this.app.route("/").get((req, res) => {
